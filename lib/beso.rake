4a389db4c867b2d6a0abb5eb27eb5350350c0671
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2012-04-11 16:07:04 -0700

    Add a placeholder rake task

---
+++ lib/beso.rake
@@ -0,0 +1,6 @@
+namespace :beso do
+  desc "Testing testing 1 2 3"
+  task :test do
+    puts "YATTA!"
+  end
+end
--- lib/beso/railtie.rb
+++ lib/beso/railtie.rb
@@ -1,4 +1,7 @@
 module Beso
   class Railtie < Rails::Railtie
+    rake_tasks do
+      load 'lib/beso.rake'
+    end
   end
 end
